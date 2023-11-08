6f361f23031f75d15caa4fe69c2dde829aa270fb
Long Nguyen <long.nguyenkim@nashtechglobal.com>
2023-11-08 20:00:27 +0700

    feat: update script tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,24 +8,24 @@ import Link from 'next/link';
 
 function MyApp({ Component, pageProps }: AppProps) {
   <Head>
-    <meta charSet="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <meta httpEquiv="refresh" content="5" />
+    <meta charSet='utf-8' />
+    <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+    <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <meta name='description' content='' />
+    <meta name='author' content='' />
+    <meta httpEquiv='refresh' content='5' />
 
     <title>Clean Blog</title>
 
     <Link
-      href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic"
-      rel="stylesheet"
-      type="text/css"
+      href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic'
+      rel='stylesheet'
+      type='text/css'
     />
     <Link
-      href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"
-      rel="stylesheet"
-      type="text/css"
+      href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+      rel='stylesheet'
+      type='text/css'
     />
   </Head>;
   const router = useRouter();
@@ -33,13 +33,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} key={router.route} />
-      <script
-        async
-        src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"
-        type="text/javascript"
-      ></script>
-      <script async type="text/javascript" src="static/js/clean-blog.min.js" />
-      <script async type="text/javascript" src="static/js/bootstrap.min.js" />
+      <script async type='text/javascript' src='static/js/jquery-1.12.4.min.js' />
+      <script async type='text/javascript' src='static/js/clean-blog.min.js' />
+      <script async type='text/javascript' src='static/js/bootstrap.min.js' />
     </>
   );
 }
